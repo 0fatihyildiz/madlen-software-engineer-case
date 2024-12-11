89c97a3e5f3dbfe77a172dd228f0de014b0ce202
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2024-12-12 01:50:50 +0300

    feat: refactor TableHeader to use a custom hook for debounced filter values; improve filter handling and UI responsiveness

--- client/src/components/TableHeader.tsx
+++ client/src/components/TableHeader.tsx
@@ -1,3 +1,4 @@
+import type { Question } from '../types/question'
 import { Cross1Icon, MagnifyingGlassIcon } from '@radix-ui/react-icons'
 import { Badge, Button, Flex, Select, TextField } from '@radix-ui/themes'
 import { useQuery } from '@tanstack/react-query'
@@ -6,75 +7,32 @@ import { COGNITIVE_LEVEL, COURSE_NAME, DIFFICULTY_LEVEL } from '../constants/tab
 import { GeneralContext } from '../context/GeneralContext'
 import fetcher from '../utils/api'
 
-function TableHeader() {
-    const { setQuestions, fetchQuestions } = useContext(GeneralContext)
-
-    const [filters, setFilters] = useState({
-        course_name: '',
-        difficulty_level: '',
-        cognitive_level: '',
-        question_text: '',
-        context_name: '',
-    })
-
-    const [debouncedFilters, setDebouncedFilters] = useState(filters)
+function useDebouncedValue<T>(value: T, delay: number): T {
+    const [debouncedValue, setDebouncedValue] = useState(value)
 
     useEffect(() => {
-        const handler = setTimeout(() => {
-            setDebouncedFilters(filters)
-        }, 500)
+        const handler = setTimeout(() => setDebouncedValue(value), delay)
         return () => clearTimeout(handler)
-    }, [filters])
+    }, [value, delay])
 
-    const isAnyFilterActive = Object.values(debouncedFilters).some(value => value.trim() !== '')
+    return debouncedValue
+}
 
-    async function fetchQuestionsWithFilter(filters: Record<string, string>) {
-        return fetcher(`/questions?${new URLSearchParams(filters).toString()}`, {
-            method: 'GET',
-        })
-    }
-
-    const { data, error, isLoading } = useQuery({
-        queryKey: ['questions', debouncedFilters],
-        queryFn: () => fetchQuestionsWithFilter(debouncedFilters),
-        enabled: isAnyFilterActive,
-        staleTime: 5000,
-    })
-
-    useEffect(() => {
-        if (!error && !isLoading && data) {
-            setQuestions?.(data)
-        }
-    }, [data, error, isLoading, setQuestions])
-
-    useEffect(() => {
-        if (isAnyFilterActive && fetchQuestions) {
-            fetchQuestions()
-        }
-    }, [isAnyFilterActive, fetchQuestions])
-
-    const handleFilterChange = (key: string, value: string) => {
-        setFilters(prev => ({ ...prev, [key]: value }))
-    }
-
-    const handleClearFilters = () => {
-        setFilters({
-            course_name: '',
-            difficulty_level: '',
-            cognitive_level: '',
-            question_text: '',
-            context_name: '',
-        })
-    }
-
-    const renderSelect = (key: string, placeholder: string, options: string[]) => (
-        <Select.Root onValueChange={value => handleFilterChange(key, value)}>
+function selectOptions(
+    key: keyof typeof initialFilters,
+    placeholder: string,
+    options: string[],
+    handleChange: (key: string, val: string) => void,
+    value: string,
+) {
+    return (
+        <Select.Root value={value} onValueChange={newValue => handleChange(key, newValue)}>
             <Select.Trigger className="capitalize" placeholder={placeholder} />
-            <Select.Content className="h-auto">
+            <Select.Content>
                 <Select.Group>
                     <Select.Label>{placeholder}</Select.Label>
                     {options.map(option => (
-                        <Select.Item key={option} className="capitalize" value={option}>
+                        <Select.Item key={option} className="capitalize" value={option.at(0)?.toUpperCase() + option.slice(1)}>
                             {option}
                         </Select.Item>
                     ))}
@@ -82,11 +40,57 @@ function TableHeader() {
             </Select.Content>
         </Select.Root>
     )
+}
+
+const initialFilters = {
+    course_name: '',
+    difficulty_level: '',
+    cognitive_level: '',
+    question_text: '',
+    context_name: '',
+}
+
+function TableHeader() {
+    const { setQuestions, fetchQuestions } = useContext(GeneralContext)
+
+    const [filters, setFilters] = useState(initialFilters)
+    const debouncedFilters = useDebouncedValue(filters, 500)
+    const isAnyFilterActive = Object.values(debouncedFilters).some(val => val.trim() !== '')
+
+    const handleFilterChange = (key: string, value: string) => {
+        setFilters(prev => ({ ...prev, [key]: value }))
+    }
+
+    const handleClearFilters = () => {
+        setFilters(initialFilters)
+        fetchQuestions()
+    }
+
+    const fetchQuestionsWithFilter = async (filters: Record<string, string>) => {
+        const query = new URLSearchParams(filters).toString()
+        return fetcher(`/questions?${query}`, { method: 'GET' })
+    }
+
+    const { data } = useQuery<Question[], Error>({
+        queryKey: ['questions', debouncedFilters],
+        queryFn: () => fetchQuestionsWithFilter(debouncedFilters),
+        enabled: isAnyFilterActive,
+        staleTime: 5000,
+    })
+
+    useEffect(() => {
+        setQuestions?.(data)
+    }, [data, setQuestions])
 
     return (
         <Flex direction="row" gap="2" align="center" justify="between" className="w-full">
             <Flex direction="row" gap="2" align="center">
-                <img src="/logo.png" alt="logo" className="h-[2.0625rem] w-[9.6875rem]" draggable="false" />
+                <img
+                    src="/logo.png"
+                    alt="logo"
+                    className="h-[1.5rem] w-[7rem]"
+                    draggable="false"
+                />
                 <Badge color="gray">Case</Badge>
             </Flex>
 
@@ -94,19 +98,24 @@ function TableHeader() {
                 <TextField.Root
                     className="w-[5.625rem]"
                     placeholder="Context N..."
+                    value={filters.context_name}
                     onChange={e => handleFilterChange('context_name', e.target.value)}
                 />
-                {renderSelect('course_name', 'Course Name', COURSE_NAME)}
-                {renderSelect('difficulty_level', 'Difficulty Level', DIFFICULTY_LEVEL)}
-                {renderSelect('cognitive_level', 'Cognitive Level', COGNITIVE_LEVEL)}
+
+                {selectOptions('course_name', 'Course Name', COURSE_NAME, handleFilterChange, filters.course_name)}
+                {selectOptions('difficulty_level', 'Difficulty Level', DIFFICULTY_LEVEL, handleFilterChange, filters.difficulty_level)}
+                {selectOptions('cognitive_level', 'Cognitive Level', COGNITIVE_LEVEL, handleFilterChange, filters.cognitive_level)}
+
                 <TextField.Root
                     placeholder="Search the question…"
+                    value={filters.question_text}
                     onChange={e => handleFilterChange('question_text', e.target.value)}
                 >
                     <TextField.Slot>
                         <MagnifyingGlassIcon height="16" width="16" />
                     </TextField.Slot>
                 </TextField.Root>
+
                 {isAnyFilterActive && (
                     <Button variant="soft" color="gray" onClick={handleClearFilters}>
                         <Cross1Icon />
